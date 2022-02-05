e5fdf20ccc8200c882144a12cf47b16355f72621
JUMAHNAH <salampilakul@gmail.com>
2022-02-05 17:29:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// <form action="" class="form" onsubmit="test()">
+//         <input type="text" name="NAME" class="input" placeholder="Enter your Name">
+//         <input type="email" name="EMAIL" class="input" placeholder="Enter your Email"> 
+//         <textarea name="MESSAGE" id="MESSAGE" cols="70" rows="20" placeholder="Enter your Message"></textarea> 
+//    <input type="submit" value="SUBMIT" id="SUBMIT">
+   
+//     </form>
+//     <script>
+//     function test() {
+//       alert(`The function 'test' is executed`);
+//     }
+//   </script>
+const form=document.getElementById('SUBMIT')
+form.addEventListener('SUBMIT', (event) => {
+ 
+
+});
+form.SUBMIT();
